4d140e8ee4eb8c15170b6dc1c2af606bd675ab6c
SURESH KUMAR <99707272+SURESKUMARPRAJAPATI@users.noreply.github.com>
2022-02-19 21:34:16 +0530

    Add files via upload

---
+++ AdditionToNumber/AdditionToNumber/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AdditionToNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter the first No.");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second No.");
+            int b = int.Parse(Console.ReadLine());
+           int sum = a + b;
+            Console.WriteLine("Sum is : = " + sum);
+        }
+    }
+}
