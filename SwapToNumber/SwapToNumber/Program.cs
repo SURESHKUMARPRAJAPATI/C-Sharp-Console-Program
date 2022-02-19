fae83b94b1d421e71ea091585995a6e9dd9e8d31
SURESH KUMAR <99707272+SURESKUMARPRAJAPATI@users.noreply.github.com>
2022-02-19 22:28:05 +0530

    Add files via upload

---
+++ SwapToNumber/SwapToNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SwapToNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first Number");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second Number");
+            int b = int.Parse(Console.ReadLine());
+            a = a - b;
+            b = a + b;
+            a = b - a;
+            Console.WriteLine("After the swapping Number is " + a);
+            Console.WriteLine("After the swapping Number is " + b);
+        }
+    }
+}
