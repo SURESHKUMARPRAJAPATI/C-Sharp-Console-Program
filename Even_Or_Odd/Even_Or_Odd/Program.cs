ecad25116754466b766e10f988edb237596eb714
SURESH KUMAR <99707272+SURESKUMARPRAJAPATI@users.noreply.github.com>
2022-02-20 10:09:37 +0530

    Add files via upload

---
+++ Even_Or_Odd/Even_Or_Odd/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Even_Or_Odd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter s Number");
+            int num = int.Parse(Console.ReadLine());
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("It is an even Number");
+            }
+            else
+            {
+                Console.WriteLine("It is an odd Number");
+            }
+        }
+    }
+}
