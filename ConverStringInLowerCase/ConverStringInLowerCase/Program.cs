b6ec5aaa1504e306ef07ce9a3d373feb9209c5c4
SURESH KUMAR <99707272+SURESKUMARPRAJAPATI@users.noreply.github.com>
2022-02-20 10:08:28 +0530

    Add files via upload

---
+++ ConverStringInLowerCase/ConverStringInLowerCase/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConverStringInLowerCase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Sentence in Upercase");
+            string a = Console.ReadLine();
+            Console.WriteLine("The Lowercase Sentence is = " + a.ToLower());
+        }
+    }
+}
