c582d5e3e4fcdbc0f842e24b4d29eb88355835ec
SURESH KUMAR <99707272+SURESKUMARPRAJAPATI@users.noreply.github.com>
2022-02-20 11:37:41 +0530

    Add files via upload

---
+++ Sum_first_10_Number/Sum_first_10_Number/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Sum_first_10_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, sum = 0;
+            Console.WriteLine("The first 10 Natural Numbers are:");
+            for(a=1;a<=10; a++)
+            {
+               
+                 sum = sum +a;
+                Console.WriteLine("The sum of first 10 Natural Number is :");
+                Console.WriteLine(sum);
+
+            }
+        }
+    }
+}
