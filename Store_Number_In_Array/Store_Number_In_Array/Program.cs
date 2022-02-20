14d39c46ac7c783afec71878c57eaa0cbaed34ec
SURESH KUMAR <99707272+SURESKUMARPRAJAPATI@users.noreply.github.com>
2022-02-20 11:43:34 +0530

    Add files via upload

---
+++ Store_Number_In_Array/Store_Number_In_Array/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Store_Number_In_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] a =new int[10];
+            Console.WriteLine("Enter the Number");
+            for(int j=0;j<10;j++)
+            {
+                a[j]= int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Array value is");
+            foreach(int i in a)     
+                Console.WriteLine(i);
+
+        }
+    }
+}
