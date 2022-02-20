2910d414e65ece03dfa6c98f5599393f355e153f
SURESH KUMAR <99707272+SURESKUMARPRAJAPATI@users.noreply.github.com>
2022-02-20 11:42:21 +0530

    Add files via upload

---
+++ Triangle_Of_Number/Triangle_Of_Number/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Triangle_Of_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Number");
+            for(int i=1;i<=10;i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("The reverse Number is");
+            for(int j=10;j>=1;j--)
+            {
+                Console.WriteLine(j);
+            }
+        }
+    }
+}
