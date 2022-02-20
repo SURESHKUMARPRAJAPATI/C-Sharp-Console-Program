4c7e307a5d27edba34e36ad5d372d8a41877b56e
SURESH KUMAR <99707272+SURESKUMARPRAJAPATI@users.noreply.github.com>
2022-02-20 11:40:37 +0530

    Add files via upload

---
+++ Find_Largest_Number/Find_Largest_Number/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Find_Largest_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first Number");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second Number");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Third Number");
+            int c = int.Parse(Console.ReadLine());
+            {
+                if(a>b && a>c)
+                {
+                    Console.WriteLine("The largest No. is = " + a);
+                }
+                else if(b>a && b>c)
+                {
+                    Console.WriteLine("The largest No. is = " + b);
+                }
+                else
+                {
+                    Console.WriteLine("The largest No. is = " + c);
+                }
+            }
+        }
+    }
+}
