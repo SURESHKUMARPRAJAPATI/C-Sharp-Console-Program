0178eda8a174083d55c98124219d7f60489a4078
SURESH KUMAR <99707272+SURESKUMARPRAJAPATI@users.noreply.github.com>
2022-02-20 10:10:37 +0530

    Add files via upload

---
+++ Multiplication_table/Multiplication_table/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Multiplication_table
+{
+    class Multi
+
+    {
+        public void multi()
+        {
+            Console.WriteLine("Enter the any Number :");
+            int num = int.Parse(Console.ReadLine());
+            for(int i= 1; i <=10 ; i++)
+            {
+                Console.WriteLine($"{num}*{num*i}");
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Multi obj = new Multi();
+            obj.multi();
+        }
+    }
+}
