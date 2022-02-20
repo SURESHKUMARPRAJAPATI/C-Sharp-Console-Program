63d2b1727781837bf468f99603322374dbd38de1
SURESH KUMAR <99707272+SURESKUMARPRAJAPATI@users.noreply.github.com>
2022-02-20 11:36:46 +0530

    Add files via upload

---
+++ 10Natural_Number/10Natural_Number/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _10Natural_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1;i<=10;i++)
+            Console.WriteLine(i);
+        }
+    }
+}
